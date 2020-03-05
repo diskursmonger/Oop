72e5e763a6bed986c6a69ae82d670b952d807dfe
diskursmonger <57355810+diskursmonger@users.noreply.github.com>
2020-03-05 14:16:16 +0700

    Update Statistics.java

--- Statistics.java
+++ Statistics.java
@@ -4,7 +4,7 @@ public class Statistics{
     public String symb;
     int num = 0;
     public static String makeStr(String symbol,int num){
-        return "" + symbol + " occurs " + num + " times"; // "A : 2"
+        return symbol + " occurs " + num + " times"; // "A : 2"
     }
     public boolean equalSymbol(String symbol){
         return this.symb.equals(symbol);
