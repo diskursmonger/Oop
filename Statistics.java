225dad97e4476f902dd3347065e112bf2cb07bef
diskursmonger <57355810+diskursmonger@users.noreply.github.com>
2020-03-05 14:13:54 +0700

    Create Statistics.java

---
+++ Statistics.java
@@ -0,0 +1,12 @@
+package bruh;
+
+public class Statistics{
+    public String symb;
+    int num = 0;
+    public static String makeStr(String symbol,int num){
+        return "" + symbol + " occurs " + num + " times"; // "A : 2"
+    }
+    public boolean equalSymbol(String symbol){
+        return this.symb.equals(symbol);
+    }
+}
